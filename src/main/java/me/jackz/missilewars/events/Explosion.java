2ffe42f9f8a03dec8b26de253786c2ad76d1f801
Jackz <me@jackz.me>
2020-05-11 09:22:44 -0500

    Added win test condition

---
+++ src/main/java/me/jackz/missilewars/events/Explosion.java
@@ -0,0 +1,31 @@
+package me.jackz.missilewars.events;
+
+import com.sk89q.worldedit.bukkit.BukkitAdapter;
+import com.sk89q.worldedit.math.BlockVector3;
+import com.sk89q.worldedit.world.World;
+import com.sk89q.worldguard.WorldGuard;
+import com.sk89q.worldguard.protection.ApplicableRegionSet;
+import me.jackz.missilewars.MissileWars;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockExplodeEvent;
+
+public class Explosion implements Listener {
+
+    @EventHandler
+    public void onExplosion(BlockExplodeEvent e) {
+        if(MissileWars.gameManager.getState().isGameActive()) {
+            World world = BukkitAdapter.adapt(e.getBlock().getWorld());
+            for (Block block : e.blockList()) {
+                if (block.getType().equals(Material.NETHER_PORTAL)) {
+                    BlockVector3 blockVector3 = BukkitAdapter.asBlockVector(block.getLocation());
+                    ApplicableRegionSet regions = WorldGuard.getInstance().getPlatform().getRegionContainer().get(world).getApplicableRegions(blockVector3);
+                    MissileWars.gameManager.testWin(regions);
+                    break;
+                }
+            }
+        }
+    }
+}
