0da44366daf5d5342f1ad2eff3f25ef5fdba8dab
Jackz <me@jackz.me>
2020-05-11 13:03:20 -0500

    Added game config

---
+++ src/main/java/me/jackz/missilewars/game/GameConfig.java
@@ -0,0 +1,23 @@
+package me.jackz.missilewars.game;
+
+public class GameConfig {
+    private int item_interval_sec = 5;
+    private boolean prioritize_defense = false;
+    private int max_items = 1;
+
+    public void reload() {
+        //load config
+    }
+
+    public int getItemInterval() {
+        return item_interval_sec;
+    }
+
+    public boolean prioritizeDefenseEnabled() {
+        return prioritize_defense;
+    }
+
+    public int getMaxItems() {
+        return max_items;
+    }
+}
