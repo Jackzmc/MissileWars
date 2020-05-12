227e42cc6d5774e805cd8fec885037a077efb705
Jackz <me@jackz.me>
2020-05-12 15:32:56 -0500

    Made wins actually be detected - Changed BlockExplodeEvent to EntityExplodeEvent

--- src/main/java/me/jackz/missilewars/events/Explosion.java
+++ src/main/java/me/jackz/missilewars/events/Explosion.java
@@ -5,20 +5,25 @@ import com.sk89q.worldedit.math.BlockVector3;
 import com.sk89q.worldedit.world.World;
 import com.sk89q.worldguard.WorldGuard;
 import com.sk89q.worldguard.protection.ApplicableRegionSet;
+import com.sk89q.worldguard.protection.regions.ProtectedRegion;
 import me.jackz.missilewars.MissileWars;
 import me.jackz.missilewars.game.WinManager;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockExplodeEvent;
+import org.bukkit.event.entity.EntityExplodeEvent;
+
+import java.util.stream.Collectors;
 
 public class Explosion implements Listener {
 
     @EventHandler
-    public void onExplosion(BlockExplodeEvent e) {
+    public void onExplosion(EntityExplodeEvent e) {
         if(MissileWars.gameManager.getState().isGameActive()) {
-            World world = BukkitAdapter.adapt(e.getBlock().getWorld());
+            World world = BukkitAdapter.adapt(e.getLocation().getWorld());
             for (Block block : e.blockList()) {
                 if (block.getType().equals(Material.NETHER_PORTAL)) {
                     BlockVector3 blockVector3 = BukkitAdapter.asBlockVector(block.getLocation());
