e59a40e18f8bf4745eb4dd38bfd00470ac940b65
Jackz <me@jackz.me>
2020-05-12 14:00:30 -0500

    Implement start logic, more reset logic, and shutdown logic

--- src/main/java/me/jackz/missilewars/game/GameManager.java
+++ src/main/java/me/jackz/missilewars/game/GameManager.java
@@ -6,19 +6,30 @@ import me.jackz.missilewars.MissileWars;
 import net.md_5.bungee.api.ChatMessageType;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scoreboard.Scoreboard;
 import org.bukkit.scoreboard.Team;
+
+import java.util.List;
 
 public class GameManager {
     private MissileWars plugin;
     private GameState state;
     private GamePlayers players;
     private static GameConfig config;
+    private ItemSystem itemSystem;
+
+    private final static PotionEffect nightVision = new PotionEffect(PotionEffectType.NIGHT_VISION, 9999, 2, true, false, false);
+    private final static PotionEffect saturation = new PotionEffect(PotionEffectType.SATURATION, 9999, 2, true, false, false);
 
     public GameManager(MissileWars plugin) {
         this.state = new GameState();
         this.plugin = plugin;
+        itemSystem = new ItemSystem();
         players = new GamePlayers();
         config = new GameConfig();
         initalizeScoreboard();
@@ -48,14 +59,42 @@ public class GameManager {
         if(main.getObjective("loses") == null ) main.registerNewObjective("loses", "dummy", "Loses");
     }
     //#endregion
-
+    public void start() {
+        state.setActive(true);
+        ItemStack bow = ItemSystem.getItem("bow");
+        for (Player player : players.getAll()) {
+            player.setGameMode(GameMode.SURVIVAL);
+            player.getInventory().clear();
+            player.addPotionEffect(nightVision);
+            player.addPotionEffect(saturation);
+            //todo: tp to spawnpoint
+            player.setHealth(20);
+            ItemSystem.giveItem(player, bow, 1);
+        }
+        itemSystem.start();
+    }
     public void reset() {
+        for (Player player : players.getAll()) {
+            player.setGameMode(GameMode.SPECTATOR);
+            player.getInventory().clear();
+            player.addPotionEffect(nightVision);
+            player.addPotionEffect(saturation);
+            player.setHealth(20);
+        }
+        state.setActive(false);
         Reset.reset();
+        itemSystem.stop();
         //todo: run reset, copy regions, and reset gamestate, and players list
     }
 
     public void shutdown() {
-
+        state = null;
+        if(itemSystem != null) {
+            itemSystem.stop();
+            itemSystem = null;
+        }
+        players = null;
+        config = null;
     }
 
     public GameState getState() {
