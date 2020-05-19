9b14bb4285ce03e3357fe04fc11afa8428e2b75a
Jackz <me@jackz.me>
2020-05-18 20:34:02 -0500

    Added /game stats command & minor cmd tweaks

--- src/main/java/me/jackz/missilewars/commands/GameCommand.java
+++ src/main/java/me/jackz/missilewars/commands/GameCommand.java
@@ -3,6 +3,9 @@ package me.jackz.missilewars.commands;
 import me.jackz.missilewars.MissileWars;
 import me.jackz.missilewars.game.GameManager;
 import me.jackz.missilewars.game.GamePlayers;
+import me.jackz.missilewars.lib.StatsTracker;
+import net.md_5.bungee.api.chat.BaseComponent;
+import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.OfflinePlayer;
@@ -11,8 +14,10 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.Player;
-import org.bukkit.scoreboard.Scoreboard;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -27,31 +32,46 @@ public class GameCommand implements CommandExecutor  {
      */
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if(args.length >= 2 && args[0].equalsIgnoreCase("join")) {
-            Player player;
-            if(args.length >= 3) {
-                player = Bukkit.getPlayer(args[3]);
-                if(player == null) {
-                    sender.sendMessage("§cCould not find that player");
+        if(args.length >= 1) {
+            if(args[0].equalsIgnoreCase("join")) {
+                if(args.length < 2) {
+                    sender.sendMessage("§cUsage: /game join <red/green>");
                     return true;
                 }
-            }else{
-                if(sender instanceof Player) {
-                    player = (Player) sender;
-                }else{
-                    sender.sendMessage("§cCan not use this command in console. You can specify a player instead");
-                    return true;
+                Player player;
+                if (args.length >= 3) {
+                    player = Bukkit.getPlayer(args[3]);
+                    if (player == null) {
+                        sender.sendMessage("§cCould not find that player");
+                        return true;
+                    }
+                } else {
+                    if (sender instanceof Player) {
+                        player = (Player) sender;
+                    } else {
+                        sender.sendMessage("§cCan not use this command in console. You can specify a player instead");
+                        return true;
+                    }
                 }
+                if (args[1].equalsIgnoreCase("green")) {
+                    MissileWars.gameManager.players().joinPlayer(player, GamePlayers.MWTeam.GREEN);
+                } else if (args[1].equalsIgnoreCase("red")) {
+                    MissileWars.gameManager.players().joinPlayer(player, GamePlayers.MWTeam.RED);
+                }
+                return true;
+            }else if(args[0].equalsIgnoreCase("stats")) {
+                if(args.length >= 2) {
+                    if(args[1].equalsIgnoreCase("global")) {
+                        sender.spigot().sendMessage(getTotalStatComponents(true));
+                        return true;
+                    }else if(args[1].equalsIgnoreCase("session")) {
+                        sender.spigot().sendMessage(getTotalStatComponents(false));
+                        return true;
+                    }
+                }
+                sender.sendMessage("§cUsage: /game stats <global/session>");
+                return true;
             }
-            if (args[1].equalsIgnoreCase("green")) {
-                MissileWars.gameManager.players().joinPlayer(player, GamePlayers.MWTeam.GREEN);
-            } else if (args[1].equalsIgnoreCase("red")) {
-                MissileWars.gameManager.players().joinPlayer(player, GamePlayers.MWTeam.RED);
-            }
-            if(sender instanceof Player) {
-
-            }
-            return true;
         }
 
         //todo: fetch stat
@@ -79,7 +99,40 @@ public class GameCommand implements CommandExecutor  {
         return true;
     }
 
-    private boolean isPlayerOnline(String username) {
-        return plugin.getServer().getPlayer(username.toLowerCase()) != null;
+    private BaseComponent[] getTotalStatComponents(boolean global) {
+        StatsTracker stats = GameManager.getStats();
+        int green_wins = stats.getStat("wins.team_green", global);
+        int red_wins = stats.getStat("wins.team_red", global);
+
+        int green_loses = stats.getStat("loses.team_green", global);
+        int red_loses = stats.getStat("loses.team_red", global);
+
+        int deaths = stats.getStat("generic.deaths.total", global);
+        int shield_spawns = stats.getStat("spawns.barrier.total", global);
+        int fireball_launches = stats.getStat("spawns.fireball.total", global);
+        int tomahawk_spawns = stats.getStat("spawns.tomahawk.total", global);
+        int juggernaut_spawns = stats.getStat("spawns.juggernaut.total", global);
+        int guardian_spawns = stats.getStat("spawns.guardian.total", global);
+        int lightning_spawns = stats.getStat("spawns.lightning.total", global);
+        int shieldbuster_spawns = stats.getStat("spawns.shieldbuster.total", global);
+        int minutes_played = stats.getStat("gametime_min.total", global);
+        int minutes_played_highest = stats.getStat("gametime_min.longest", global);
+        List<BaseComponent> components = new ArrayList<>(Arrays.asList(
+                new TextComponent("§6§n" + (global? "Session" : "Global") + " Statistics"),
+                new TextComponent(String.format("\n§aGreen -> Wins: §e%d §a| Loses: §e%d", green_wins, green_loses)),
+                new TextComponent(String.format("\n§cRed   -> Wins: §e%d §c| Loses: §e%d", red_wins, red_loses)),
+                new TextComponent(String.format("\n§9Minutes Played Total: §e%d §9(Highest per game: §e%d§9)",minutes_played, minutes_played_highest)),
+                new TextComponent("\n§9Deaths: §e" + deaths),
+                new TextComponent("\n§9Fireball Launches: §e" + fireball_launches),
+                new TextComponent("\n§9Barriers Deployed: §e" + shield_spawns),
+                new TextComponent("\n§9Tomahawks Deployed: §e" + tomahawk_spawns),
+                new TextComponent("\n§9Juggernauts Deployed: §e" + juggernaut_spawns),
+                new TextComponent("\n§9Guardians Deployed: §e" + guardian_spawns),
+                new TextComponent("\n§9Lightnings Deployed: §e" + lightning_spawns),
+                new TextComponent("\n§9Shieldbusters Deployed: §e" + shieldbuster_spawns)
+        ));
+        BaseComponent[] componentArray = new BaseComponent[components.size()];
+        components.toArray(componentArray);
+        return componentArray;
     }
 }
