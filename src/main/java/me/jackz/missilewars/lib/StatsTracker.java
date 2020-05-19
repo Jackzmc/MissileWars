95d9255d71d88792cd9725e607252edc7599622e
Jackz <me@jackz.me>
2020-05-18 19:14:33 -0500

    Replace check if valid UUID check

--- src/main/java/me/jackz/missilewars/lib/StatsTracker.java
+++ src/main/java/me/jackz/missilewars/lib/StatsTracker.java
@@ -49,7 +49,7 @@ public class StatsTracker {
                 String id = key.substring(key.lastIndexOf(".") + 1);
                 int value = stringIntegerEntry.getValue();
 
-                if (!id.equalsIgnoreCase("total")) {
+                if (id.matches("\"[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}\"")) {
                     if (value > highest) {
                         highest = value;
                         name = id;
