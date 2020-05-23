969ccfad9e22e346bad237a93bde126260980ea8
Jackz <me@jackz.me>
2020-05-23 17:00:25 -0500

    Removed legacy_missiles option & set default state to off

--- src/main/java/me/jackz/missilewars/game/GameState.java
+++ src/main/java/me/jackz/missilewars/game/GameState.java
@@ -3,8 +3,7 @@ package me.jackz.missilewars.game;
 public class GameState {
 
 
-    private boolean activeState = true;
-    private boolean legacy_missiles = false;
+    private boolean activeState = false;
     private boolean debug_enabled = false;
 
     private boolean redTeamReady = false;
@@ -19,14 +18,8 @@ public class GameState {
     public boolean isDebug() {
         return debug_enabled;
     }
-    public boolean isLegacyMissilesEnabled() {
-        return legacy_missiles;
-    }
     //#endregion
     //#region setters
-    public void setLegacyMissiles(boolean legacy_missiles) {
-        this.legacy_missiles = legacy_missiles;
-    }
     public void setActive(boolean bool) {
         activeState = bool;
     }
